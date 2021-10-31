915191827a0b0f40c5742479b04e090385a3391a
stonebridge <stonebridge@njfu.edu.cn>
2021-10-31 20:43:23 +0800

    4.动态SQL 环境准备

---
+++ mybatis-dynamic-SQL/src/test/java/MyBastisTestImprove.java
@@ -0,0 +1,24 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+
+import java.io.IOException;
+
+public class MyBastisTestImprove {
+    private SqlSession session;
+
+    //junit会在每个@Test方法执行前执行
+    @Before
+    public void init() throws IOException {
+        session = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml")).openSession();
+    }
+
+
+    @After
+    public void clear() {
+        session.commit();
+        session.close();
+    }
+}
