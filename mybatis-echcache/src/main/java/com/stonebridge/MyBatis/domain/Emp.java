670b64e59a21cb1228712e2b2ba034914b27e33a
stonebridge <stonebridge@njfu.edu.cn>
2021-11-03 21:38:10 +0800

    5.4.整合EHCache

---
+++ mybatis-echcache/src/main/java/com/stonebridge/MyBatis/domain/Emp.java
@@ -0,0 +1,26 @@
+package com.stonebridge.MyBatis.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * 和数据库表t_emp对应的实体类
+ * emp_id INT AUTO_INCREMENT
+ * emp_name CHAR(100)
+ * emp_salary DOUBLE(10,5)
+ * <p>
+ * Java的实体类中，属性的类型不要使用基本数据类型，要使用包装类型。因为包装类型可以赋值为null，表示空，而基本数据类型不可以。
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class Emp implements Serializable {
+    private Long empId;
+    private String empName;
+    private Double empSalary;
+}
